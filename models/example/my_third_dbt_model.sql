0aea766fec21d3920788c8c7089fd0ce4fe2c574
Alex McManus <mcmanusalex99@gmail.com>
2025-12-17 11:46:00 +0000

    RESOLVE CONFLICT

--- models/example/my_third_dbt_model.sql
+++ models/example/my_third_dbt_model.sql
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-{{
-    config(
-        materialized='view'
-    )
-}}
-select *
-from {{ ref('my_second_dbt_model') }}
-=======
 {{
     config(
         materialized='view'
@@ -16,4 +7,3 @@ from {{ ref('my_second_dbt_model') }}
 select * from {{ ref('my_second_dbt_model') }}
 
 -- another comment
->>>>>>> 4bd533462b3191a6b7c0ed4b65c2601a1c6c7577

ff2b341ef8a3b66e3f47e1c9b68cb255a2bf72e6
Ollie Clarke <oliver.clarke@theinformationlab.co.uk>
2025-12-17 11:02:19 +0000

    commented codegen

--- analyses/codegen.sql
+++ analyses/codegen.sql
@@ -1,6 +1,8 @@
+{#
 {% set models_to_generate = codegen.get_models(directory='staging.jaffle_shop') %}
 
 {{ codegen.generate_model_yaml(
     model_names = models_to_generate,
     upstream_descriptions = true
 ) }}
+#}

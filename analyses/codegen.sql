1ab36089a10af752fe45ca94e62d1a71e1e6a991
Jeffrey Thompson <jeffreybthompson@sky.com>
2025-12-17 11:02:31 +0000

    smacked that akon style (got rid of the jinja)

--- analyses/codegen.sql
+++ analyses/codegen.sql
@@ -1,6 +1,9 @@
+{#
 {% set models_to_generate = codegen.get_models(directory='staging.jaffle_shop') %}
 
 {{ codegen.generate_model_yaml(
     model_names = models_to_generate,
     upstream_descriptions = true
 ) }}
+
+#}
